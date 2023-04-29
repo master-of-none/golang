4591814490911a06d268d9205883fd7084675413
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-04-29 14:43:18 -0700

    Exercise 1.2

---
+++ exercise/chapter1/ex2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	sep := ""
+	i := 1
+	for _, args := range os.Args[1:] {
+		sep = args
+		fmt.Println("Index:", i, "Args", sep)
+		i++
+	}
+}
